3db4965a6cad2e36e7780c0a3acd14a22dadd207
yaba12219 <yabahane@gmail.com>
2022-02-15 05:34:41 +0300

    first commit

---
+++ HTML , CSS & JAVASCRIPT/intro-component-with-signup-form-master/script.js
@@ -0,0 +1,30 @@
+// const form = document.querySelector('.form');
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+// const first_name = document.querySelector('#first_name');
+// const last_name = document.querySelector('#last_name');
+// const error = document.getElementsByClassName(error);
+// const first_name_error = document.querySelector('.text_error ');
+// const last_name_error = document.querySelector('.last_name_error ');
+// const email_error = document.querySelector('.email_error ');
+// const password_error = document.querySelector('.password_error ');
+
+// form.addEventListener("submit", function (e) {
+//   if (first_name.value === '') {
+//     error.classList.remove(first_name_error);
+//   }
+ 
+//   e.preventDefault();
+// });
+
+// function spinWords(string) {
+
+//   console.log([...string].reverse().join(""));
+//   return string.split("").reverse().join("")
+//   // console.log( [...string].reverse().join("")) ;
+  
+// }
+
+// const spin = spinWords("sroirraw wollef yeH");
+// console.log(spin)
+
